b1afac2c7cead6e4932767cb60f44470c50ade02
Sunwook Lee <85978148+sun007021@users.noreply.github.com>
2024-03-02 14:56:10 +0000

    feat: user register

---
+++ chek/domain/user/user_router.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import get_db
+from domain.user import user_crud, user_schema
+
+router = APIRouter(
+    prefix="/user",
+)
+
+
+@router.post("/", status_code=status.HTTP_204_NO_CONTENT)
+def user_create(_user_create: user_schema.UserCreate, db: Session = Depends(get_db)):
+    user_crud.create_user(db=db, user_create=_user_create)
--- chek/main.py
+++ chek/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
 
+from domain.user import user_router
 
 app = FastAPI()
+
+app.include_router(user_router.router)
