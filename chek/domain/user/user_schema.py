d03f8724ebea10559169d181b220eafc21463e70
Sunwook Lee <85978148+sun007021@users.noreply.github.com>
2024-03-03 13:13:40 +0000

    feat: register field value validate

--- chek/domain/user/user_schema.py
+++ chek/domain/user/user_schema.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, field_validator, EmailStr
 from pydantic_core.core_schema import FieldValidationInfo
-
+from fastapi import HTTPException
 #register
 class UserCreate(BaseModel):
     username: str
@@ -12,13 +12,13 @@ class UserCreate(BaseModel):
     @field_validator('username', 'password1', 'password2', 'email')
     def not_empty(cls, v):
         if not v or not v.strip():
-            raise ValueError('빈 값은 허용되지 않습니다.')
+            raise HTTPException(status_code=400, detail='빈 값은 허용되지 않습니다.')
         return v
 
     @field_validator('password2')
     def passwords_match(cls, v, info: FieldValidationInfo):
         if 'password1' in info.data and v != info.data['password1']:
-            raise ValueError('비밀번호가 일치하지 않습니다')
+            raise HTTPException(status_code=400, detail='비밀번호가 일치하지 않습니다')
         return v
 
 
